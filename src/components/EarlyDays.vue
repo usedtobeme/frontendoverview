54d5f470e5e297770eb17a9f4067a0bf6eae84bf
Ivan Amortegui <usedtobeme@debian.pangolin.home>
2017-04-05 23:47:54 -0500

    Complete readme site basic structure

---
+++ src/components/EarlyDays.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Beggining of Time</h1>
+    <h3>User is expecting information.</h3>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'early',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Front End Overview</h1>
+    <h3>How did we get <a href="#/now">here</a>?</h3>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
